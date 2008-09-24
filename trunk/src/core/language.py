da8c2c016d1dab919602d8fadbd0470e0b8c9c26
skywatcher <skywatcher@1a7641c6-fd56-0410-a7c4-eba4ea550369>
2008-09-24 03:32:40 +0000

    Unit test updated.

--- trunk/src/core/language.py
+++ trunk/src/core/language.py
@@ -57,79 +57,16 @@ class Language:
 		f.close()
 
 	def read(self, stream):
-		tokenizer = self.lexicon.compile(self.properties)
-		fsa = self.grammar.compile()
-		er = ExpressionReader(tokenizer, fsa)
+		lexical_fsa = self.lexicon.compile(self.properties)
+		grammatical_fsa = self.grammar.compile()
+		er = ExpressionReader(lexical_fsa, grammatical_fsa)
 		return er(stream)
 			
 
 
 
 def _test():
-	from bnf import Reference, Literal, EPSILON_SYMBOL
-	from lexicon import Word, Headword, Lexicon, Particle
-	from wordfilter import WordCategoryFilter, WordFilter, AttributeFilter
-
-	if True:
-		g = Grammar("Toki pona")
-		g["sentence"] = Reference("subject") + Reference("predicate")
-		g["predicate"] = Reference("transitive-verb*") + Reference("object") 
-		g["predicate"] = Reference("intransitive-verb*") 
-		g["predicate"] = Reference("pn")
-		g["subject"] = Reference("pronoun*") | Reference("noun*") + WordFilter(Particle("li", 1))
-		g["object"] = Reference("noun*")
-		g["pronoun*"] = WordCategoryFilter("pronoun")
-		g["transitive-verb*"] = WordCategoryFilter("verb", {"transitive": "y"})
-		g["intransitive-verb*"] = WordCategoryFilter("verb", {"transitive": AttributeFilter("ni", ["y"])})
-		g["noun*"] = WordCategoryFilter("noun")
-		g["noun**"] = WordCategoryFilter("noun")
-		g["pn"] = WordCategoryFilter("adjective")
-	
-		lx = Lexicon()
-		lx.add_word(Word("mi", Headword("mi", 1, "pronoun", None, "bavi")))
-		lx.add_word(Word("sina", Headword("sina", 1, "pronoun", None, "zavi")))
-		lx.add_word(Word("suli", Headword("suli", 1, "adjective", None, "kemo")))
-		lx.add_word(Word("suna", Headword("suna", 1, "noun", None, "Lakitisi")))
-		lx.add_word(Word("telo", Headword("telo", 1, "noun", None, "bocivi")))
-		lx.add_word(Word("moku", Headword("moku", 1, "verb", {"transitive": "n"}, "fucala")))
-		lx.add_word(Word("moku", Headword("moku", 2, "verb", {"transitive": "y"}, "fucalinza")))
-		lx.add_word(Word("jan", Headword("jan", 1, "noun", None, "becami")))
-		lx.add_word(Particle("li",1))
-		#import pdb
-		#pdb.set_trace()
-	
-		l = Language("tko")
-		l.name = "Toki Pona"
-		l.grammar = g
-		l.lexicon = lx
-		l.save()
-		del l
-
-	from bnf import OPTIONAL_CLOSURE
-	
-	tko = Language("tko")
-	tko.load()
-	print tko.grammar.compile()
-	print tko.read("mi moku telo")
-	print tko.read("mi moku")
-	print tko.read("suna li suli")
-	
-	aa = Language("aa")
-	aa.grammar["sentence"] = Reference("pre") * OPTIONAL_CLOSURE + Reference("main") + Reference("post") * OPTIONAL_CLOSURE
-	aa.grammar["pre"] = WordFilter(Particle("e",1)) | WordFilter(Particle("u",1))
-	aa.grammar["post"] = aa.grammar["pre"]
-	aa.grammar["main"] = WordFilter(Particle("e",1)) | WordFilter(Particle("o",1))
-	# "e can only be a main"
-	# "e e can be pre main or main post"
-	aa.lexicon.add_word(Particle("e",1))
-	aa.lexicon.add_word(Particle("o",1))
-	aa.lexicon.add_word(Particle("u",1))
-	print aa.grammar
-	print aa.grammar.compile()
-	print aa.read("e e")
-	print aa.read("e")
-#	print aa.read("o o")
-#	print aa.read("u")
+	pass
 	
 	
 
