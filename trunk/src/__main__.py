c437e38b7fb3fce1d631c259d9a335ba975ffd86
skywatcher <skywatcher@1a7641c6-fd56-0410-a7c4-eba4ea550369>
2009-08-24 20:48:18 +0000

    nomsg

---
+++ trunk/src/__main__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+PyLilac Poject
+==============
+
+U{http://pylilac.berlios.de}
+
+G{packagetree core.lect,core.lexicon,core.grammar,core.inflection}
+
+G{importgraph core.lect,core.lexicon,core.grammar,core.inflection}
+
+G{classtree core.lect.Lect,core.lexicon.Lexicon,core.grammar.Grammar,core.inflection.Inflections}
+
+G{callgraph core.grammar.Grammar.compile}
+
+@author: Paolo Olmino
+@license: U{GNU GPL GNU General Public License<http://www.gnu.org/licenses/gpl.html>}
+"""
+
+__docformat__ = "epytext en"
+
+# General info
+__version__ = "0.4"
+__author__ = "Paolo Olmino"
+__license__ = "GNU GPL v3"
+__docformat__ = "epytext en"
+
+
+import ui
+
+def run():
+	ui.run()
+
+if __name__ == "__main__":
+	run()
