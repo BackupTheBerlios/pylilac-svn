fb2b59bcb924b6f3f2951b81acbae85171deeb2a
skywatcher <skywatcher@1a7641c6-fd56-0410-a7c4-eba4ea550369>
2009-08-24 21:11:21 +0000

    Packetizing.

---
+++ trunk/src/opt/purepython.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+A module for using I{pylilac} on native platforms, such as IronPython.
+
+It is hidden by the L{utilities} module, which tries to load the standard Python libraries and then loads from a module called {purepython}.
+The module has not been implemented, but only these specifications are released for documentation purposes.
+"""
+
+# General info
+__version__ = "0.4"
+__author__ = "Paolo Olmino"
+__license__ = "GNU GPL v3"
+__docformat__ = "epytext en"
+
+import re
+
+def reader(file):
+	"""
+	Create a CSV reader function from a file.
+	
+	At each iteration, C{next} returns the comma separated values.
+	
+	@param file: A binary readable (C{"rb"}) file object.
+	@type file: file
+	@return: A reader functions
+	@rtype: iterator
+	"""
+	return _CsvReader(file)
+
+def writer(file):
+	"""
+	Create a CSV writer function from a file.
+	
+	At each iteration, the method C{writerow} accepts the comma separated values and writes it.
+	
+	@param file: A binary writable (C{"wb"}) file object.
+	@type file: file
+	@return: A writer functions
+	@rtype: object with a C{writerow} method
+	"""
+	return _CsvReader(file)
+
+class _CsvReader:
+	"""
+	Simple CSV reader
+	"""
+	R = re.compile(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))")
+	def __init__(self, file):
+		self.__file = file
+	def __iter__(self):
+		return self
+	def next(self):
+		row = self.__file.next()
+		s = self.R.split(row)
+		return s
+
+class _CsvWriter:
+	"""
+	Simple CSV writer
+	"""
+	def __init__(self, file):
+		self.__file = file
+	def writerow(self, tuple):
+		self.__file.write(",".join(tuple))
+		self.__file.flush()
+
+class GzipFile(file):
+	"""
+	A file class implementing GZ compression.
+	"""
+	pass
+
+	
+
+
+def __test():
+	f = open("..\\data\\Latejami.csv", "rb")
+	rd = _CsvReader(f)
+	print rd.next()[0]
+	print list(rd.next())
+	print list(rd.next())
+	for (i, p, a, m, b, d, n, r) in rd:
+		print i, p, a, m, b, d, n, r
+
+if __name__ == "__main__":
+	__test()
---
+++ trunk/src/res/interlingua.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+A module to create Latejami language file.
+"""
+
+from core.interlingua import Interlingua, Concept
+import csv
+import sys
+
+def run():
+	def build(i, p, a, m, b, d, n, r):
+		c = Concept(i, p, a, m, b, d)
+		c.notes = n
+		c.reference = r
+		return c
+	l = Interlingua("data/Latejami.csv")
+	#l.p_o_s = ["N", "V", "A", "D", "C"]
+	#l.arg_struct = ["0-n"]
+	#l.arg_struct += [(a + f + "-" + d) for a in ("P", "AP", "A/P") for f in ("", "/F") for d in ("s","d")]
+	#l.arg_struct += [(a + "-" + d) for a in ("F", "F/P") for d in ("s", "d")]
+	t = l.taxonomy
+	
+	filename = "data/Latejami2.csv"
+	
+	reader = csv.reader(open(filename, "rb"))
+	l.name = reader.next()[0]
+	l.p_o_s = list(reader.next())
+	l.arg_struct = list(reader.next())
+	try:
+		for (i, pos, as, m, bc, d, n, r) in reader:
+			if bc == "":
+				bc = None
+			c = build(i, pos, as, m, bc, d, n, r)
+			t.set(c)
+	except Error, e:
+		sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
+		
+	print "Now saving."
+	l.save()
+
+	writer = csv.writer(open("data/Latejami3.csv", "wb"))
+	writer.writerow((l.name, ))
+	writer.writerow(l.p_o_s)
+	writer.writerow(l.arg_struct)
+	for c in l.taxonomy:
+		writer.writerow((c.interlingua, c.p_o_s, c.arg_struct, c.meaning, c.baseconcept, c.derivation, c.notes, c.reference))
+
+
+
+if __name__ == "__main__":
+	run()
+
