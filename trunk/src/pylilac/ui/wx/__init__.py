60d8f3d136c5834cc32d61d4ae93048c1a7301b9
skywatcher <skywatcher@1a7641c6-fd56-0410-a7c4-eba4ea550369>
2010-09-30 20:09:29 +0000

    Shell_UI

---
+++ trunk/src/pylilac/ui/wx/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Module for the wxPython graphic user interface.
+
+@author: Paolo Olmino
+@license: U{GNU GPL GNU General Public License<http://www.gnu.org/licenses/gpl.html>}
+@version: Alpha 0.1.6
+"""
+
+__docformat__ = "epytext en"
